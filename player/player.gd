26ccce4939cd74ee21f989f962290bb63a3dffcf
Juuhey <jujupantel@gmail.com>
2024-01-23 12:40:29 +0100

    Mouvements simples (haut bas gauche droite)
    Mouvements haut bas gauche droite du personnage (sprite de base donc la pdp licorne)

---
+++ player/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@onready var sprite_2d = $Sprite2D
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	#if not is_on_floor():
+	#	velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
+	
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
