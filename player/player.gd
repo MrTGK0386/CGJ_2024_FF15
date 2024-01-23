363ba8ecab24d402056e1340acaebd36d551c4d6
Juuhey <jujupantel@gmail.com>
2024-01-23 13:25:51 +0100

    mouvements pixel perfect ils marchent
    8 way movements avec max speed, accel et friction

--- player/player.gd
+++ player/player.gd
@@ -1,31 +1,32 @@
 extends CharacterBody2D
 
+const max_speed = 400
+const accel = 1500
+const friction = 600
 
-const SPEED = 300.0
-@onready var sprite_2d = $Sprite2D
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var input = Vector2.ZERO
 
+func _physics_process(delta):
+	player_movement(delta)
+	
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
 
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
-		$AnimatedSprite2D.play()
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
 	else:
-		$AnimatedSprite2D.stop()
-
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(max_speed)
+		
 	move_and_slide()
-
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
+		
+		
 	
